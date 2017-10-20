fb7f0db7bed0c0eb08c86b04a91e3b4608bfb222
Jyotika <jyotica.kondalkar@gmail.com>
2017-10-20 08:52:05 +0530

    Generated component HomepageComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ContactUsComponent } from './component/contact-us/contact-us.component';
 import { AboutUsComponent } from './component/about-us/about-us.component';
+import { HomePageComponent } from './component/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactUsComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule
